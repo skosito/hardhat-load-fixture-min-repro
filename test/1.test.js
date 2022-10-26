6b08e041a12942ace64bce91388647e02bd1b582
Stefan Kostic <skostic9242@gmail.com>
2022-10-26 23:48:50 +0200

    Move all to 1 describe

--- test/1.test.js
+++ test/1.test.js
@@ -2,12 +2,24 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const fixtures = require("./fixtures");
 
 describe("1.test", function () {
-  it("2", async () => {
+  it("1", async () => {
     await loadFixture(
       fixtures.fixture1
     );
   });
+  it("2", async () => {
+    await loadFixture(
+      fixtures.fixture2
+    );
+  });
+
   it("3", async () => {
+    await loadFixture(
+      fixtures.fixture1
+    );
+  });
+
+  it("4", async () => {
     await loadFixture(
       fixtures.fixture2
     );
